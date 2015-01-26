a2d8ff1e47141045f8d6fe4f615fbe4388996be5
Alexey Kutepov <alkutepov@gmail.com>
2015-01-26 13:48:05 +0400

    Интерфейсы

---
+++ src/Percolation.java
@@ -0,0 +1,50 @@
+/**
+ * Created by Alexey Kutepov on 26.01.15.
+ */
+public class Percolation {
+
+  public Percolation(int N) {
+
+  }
+
+  /**
+   * Open site [i, j]
+   * @param i - row
+   * @param j - column
+   */
+  public void open(int i, int j) {
+
+  }
+
+  /**
+   * is site (row i, column j) open?
+   * @param i - row
+   * @param j - column
+   * @return true - site is open, else - false
+   */
+  public boolean isOpen(int i, int j)  {
+    return false;
+  }
+
+  /**
+   * is site (row i, column j) full?
+   * @param i - row
+   * @param j - column
+   * @return true - site is full, else - false
+   */
+  public boolean isFull(int i, int j) {
+    return false;
+  }
+
+  /**
+   * is system percolated?
+   * @return true - system is percolated, else - false
+   */
+  public boolean percolates() {
+    return false;
+  }
+
+  public static void main(String[] args) {
+    // write your code here
+  }
+}
---
+++ src/PercolationStats.java
@@ -0,0 +1,50 @@
+/**
+ * Created by Alexey Kutepov on 26.01.15.
+ */
+public class PercolationStats {
+  /**
+   * perform T independent experiments on an N-by-N grid
+   * @param N
+   * @param T
+   */
+  public PercolationStats(int N, int T) {
+
+  }
+
+  /**
+   * sample mean of percolation threshold
+   * @return
+   */
+  public double mean() {
+    return 0;
+  }
+
+  /**
+   * sample standard deviation of percolation threshold
+   * @return
+   */
+  public double stddev() {
+    return 0;
+  }
+
+  /**
+   * low  endpoint of 95% confidence interval
+   * @return
+   */
+  public double confidenceLo() {
+    return 0;
+  }
+
+  /**
+   * high endpoint of 95% confidence interval
+   * @return
+   */
+  public double confidenceHi() {
+    return 0;
+  }
+
+  public static void main(String[] args) {
+
+  }
+
+}
